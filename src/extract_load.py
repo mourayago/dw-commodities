770f83d4ea0d629df00b2f94c404195140275bbb
mourayago <moura.yagoibmec@gmail.com>
2025-01-21 18:48:47 -0300

    ajuste da função de extração dos dados de tickers

--- src/extract_load.py
+++ src/extract_load.py
@@ -2,23 +2,50 @@ import yfinance as yf
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER_PROD")
+DB_PASS = os.getenv("DB_PASS_PROD")
+DB_HOST = os.getenv("DB_HOST_PRD")
+DB_PORT = os.getenv("DB_PORT_PROD")
+DB_NAME = os.getenv("DB_NAME_PROD")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 
 commodities = {'CL=F', 'GC=F', 'SI=F'}
 
-def search_commodities_data(ticker, period='5d',interval='1d'):
-    ticker = yf.Ticker(ticker)
-    data = ticker.history(period=period,interval=interval)[['Close']]
-    data['symbol'] = ticker
+# Função para buscar dados de uma commodity
+def search_commodities_data(ticker_symbol, lookback_days=15):
+    """
+    Busca os dados históricos de um ticker específico para os últimos X dias.
+    """
+    ticker = yf.Ticker(ticker_symbol)
+    data = ticker.history(period=f'{lookback_days+1}d', interval='1d')[['Close']]  # Obtém pelo menos 1 dia extra para segurança
+    data = data.reset_index()  # Reseta o índice para que a data seja uma coluna
+    data['Close'] = data['Close'].round(2)  # Arredonda os preços para 2 casas decimais
+    data['symbol'] = ticker_symbol  # Adiciona o símbolo como string
+    data.rename(columns={'Date': 'closeddate'}, inplace=True)  # Renomeia a coluna de data
+
+    # Filtrar apenas os últimos 'lookback_days'
+    data = data.tail(lookback_days)
     return data
 
+# Função para buscar dados de todas as commodities
 def search_all_commodities():
     all_data = []
-    for simbol in commodities:
-        data = search_commodities_data(simbol)
+    for symbol in commodities:
+        data = search_commodities_data(symbol)
         all_data.append(data)
     return pd.concat(all_data, ignore_index=True)
 
+# Função para carregar os dados no banco
+def load_data(df, schema='public'):
+    df.to_sql('commodities_data', con=engine, schema=schema, index=True, if_exists='replace')
+
 if __name__ == '__main__':
     commodities_data = search_all_commodities()
-    print(commodities_data)
+    load_data(commodities_data)
